f79a59c25797af95d253a30b637634d970384ca1
higzo <ashrafhgazy9@gmail.com>
2026-04-02 09:13:59 +0200

    .

--- components/ui/SubscriptionForm.tsx
+++ components/ui/SubscriptionForm.tsx
@@ -93,7 +93,7 @@ export default function SubscriptionForm({
         whatsapp: form.whatsapp,
         screenshot: screenshotUrl,
       });
-      const res = await fetch("/", {
+const res = await fetch("/netlify-forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: body.toString(),
